1ad95a695531df34c3d94c6639a1644c6c380059
Alex Pyrgiotis <apyrgio@grnet.gr>
2013-07-15 13:47:27 +0300

    thesis: Deleted the font customization of nickie
    I'm not quite sure what this customization tries to achieve but there
    seems to be a problem with it; it uses a typewriter font as main font.
    Thus,  I decided that, unless the \setromanfont feels inadequate, I will
    erase nickie's customization (there are a whole lotta lines to comment
    out).

---
+++ thesis/src/xcache-index.h
@@ -0,0 +1,10 @@
+struct xcache {
+	...
+	uint32_t size;
+	...
+	xhash_t *entries;
+	xhash_t *rm_entries;
+	...
+};
+
+
---
+++ thesis/src/xcache-prealloc.h
@@ -0,0 +1,10 @@
+struct xcache {
+	...
+	uint32_t nr_nodes;
+	struct xq free_nodes;
+	...
+	struct xcache_entry *nodes;
+	...
+};
+
+
